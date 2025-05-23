7d776f752cde5b0d4c5a7001de7fb2b08984a854
beihai.wgw <beihai.wgw@antgroup.com>
2025-05-23 14:54:58 +0800

    add tuned config for FuseMoeTriton, BailingMoePlus, TP32

--- benchmark/kernels/fused_moe_triton/tuning_fused_moe_triton.py
+++ benchmark/kernels/fused_moe_triton/tuning_fused_moe_triton.py
@@ -423,6 +423,14 @@ def main(args: argparse.Namespace):
         topk = config.num_experts_per_tok
         intermediate_size = config.moe_intermediate_size
         shard_intermediate_size = 2 * intermediate_size // args.tp_size
+    elif config.architectures[0] in [
+            "BailingMoEForCausalLM",
+            "BailingMoeForCausalLM"
+    ]:
+        E = config.num_experts
+        topk = config.num_experts_per_tok
+        intermediate_size = config.moe_intermediate_size
+        shard_intermediate_size = 2 * intermediate_size // args.tp_size
     else:
         # Default: Mixtral
         E = config.num_local_experts
