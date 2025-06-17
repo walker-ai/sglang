PullRequest: 67 fix scaling
Merge branch 'fix_scaling of git@code.alipay.com:Theta/SGLang.git into main

https://code.alipay.com/Theta/SGLang/pull_requests/67


Reviewed-by: 北海 <beihai.wgw@antgroup.com>


* fix scaling

python/sglang/srt/models/bailing_moe.py
@@ -315,7 +315,7 @@ class BailingMoESparseMoeBlock(nn.Module):
             hidden_states=hidden_states, router_logits=router_logits
         )
 
-        final_hidden_states *= self.routed_scaling_factor
+        # final_hidden_states *= self.routed_scaling_factor
 
         if self.num_shared_experts > 0:
             final_hidden_states = final_hidden_states + shared_output